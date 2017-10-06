4984b8f47959d7ff8a2858201f94af7f10895845
atoudic936 <ghostof83@hotmail.fr>
2017-10-06 14:49:46 +0200

    mise en place de la fenetre et test des fonctionnalitées javafx.

---
+++ graphique/src/main/java/fr/univtln/doodle/d14/Test.java
@@ -0,0 +1,16 @@
+package fr.univtln.doodle.d14;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
