041fe01c13e52f90d3d844f159357a548dd92f87
Cysou <cysou3@laposte.net>
2017-10-08 11:55:26 +0200

    ajout classe Date

---
+++ serveur/src/main/java/fr/univtln/ancyen/doodle/Date.java
@@ -0,0 +1,24 @@
+package fr.univtln.ancyen.doodle;
+
+public class Date {
+    private int jour;
+    private int mois;
+    private int annee;
+    private int dureeHeure;
+
+    public Date(int jour, int mois, int annee, int dureeHeure) {
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+        this.dureeHeure = dureeHeure;
+    }
+
+    public int getDureeHeure() {
+        return dureeHeure;
+    }
+
+    @Override
+    public String toString() {
+        return jour+"/"+mois+"/"+"/"+annee;
+    }
+}
