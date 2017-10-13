dbd0ba7547cf06cc98a2716ec31438a1cdfa9667
enzosanci <enzo-sanci@etud.univ-tln.fr>
2017-10-13 16:34:14 +0200

    Modif Evenement + Realisation de Vote

---
+++ serveur/src/main/java/fr/univtln/ancyen/doodle/Vote.java
@@ -0,0 +1,32 @@
+package fr.univtln.ancyen.doodle;
+
+import fr.univtln.ancyen.doodle.date.Date;
+import fr.univtln.ancyen.doodle.utilisateur.Participant;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vote {
+
+    private Evenement evenement;
+    private Participant participant;
+    private static List<Date> Votes = new ArrayList<>();
+
+
+
+
+
+    public Vote(Evenement evenement, Participant participant List Votes ) {
+
+
+
+
+
+    }
+
+
+
+
+
+}
