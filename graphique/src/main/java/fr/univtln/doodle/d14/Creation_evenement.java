45d6f4636ab73da6818acda985092d370f0765ae
atoudic936 <ghostof83@hotmail.fr>
2017-10-19 19:33:59 +0200

    ajoute les champs de description et de localisation dans la classe Creation_evenement

--- graphique/src/main/java/fr/univtln/doodle/d14/Creation_evenement.java
+++ graphique/src/main/java/fr/univtln/doodle/d14/Creation_evenement.java
@@ -3,6 +3,7 @@ package fr.univtln.doodle.d14;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -12,12 +13,20 @@ import java.util.List;
 
 public class Creation_evenement {
     private final int MAX_DATES = 15;
+
     private Button btn_suivant = new Button("Suivant");
     private Button btn_retour = new Button("Retour");
     private Button btn_add_date = new Button("Ajouter une date");
-    private Text text_nom = new Text(100,200, "Nom de l'événement:");
+
+    private Text text_nom = new Text(100,50, "Nom de l'événement:");
+    private Text text_localisation = new Text(100, 150, "Localisation:");
+    private Text text_description = new Text(100, 250, "Description:");
     private Text text_date = new Text(600, 50, "Choisissez une date:");
-    private TextField nom_event = new TextField();
+
+    private TextField field_nom = new TextField();
+    private TextArea field_description = new TextArea();
+    private TextField field_localisation = new TextField();
+
     private List <DatePicker> calendar = new ArrayList<>();
     private DatePicker new_date = new DatePicker();
 
@@ -32,8 +41,8 @@ public class Creation_evenement {
         });
 
         btn_suivant.setOnAction(event -> {
-            if (!(nom_event.getText().equals(""))) {
-                String nom = nom_event.getText();
+            if (!(field_nom.getText().equals(""))) {
+                String nom = field_nom.getText();
                 for (DatePicker date:calendar) {
                     System.out.println(date.getEditor().getText());
                 }
@@ -48,20 +57,30 @@ public class Creation_evenement {
         });
 
         btn_suivant.setLayoutX(450); btn_suivant.setLayoutY(500);
-        btn_retour.setLayoutY(31); btn_retour.setLayoutX(939);
+        btn_retour.setLayoutX(939); btn_retour.setLayoutY(31);
         btn_add_date.setLayoutX(600); btn_add_date.setLayoutY(70);
-        nom_event.setLayoutX(100); nom_event.setLayoutY(220);
+
+        field_nom.setLayoutX(100); field_nom.setLayoutY(70);
+        field_localisation.setLayoutX(100); field_localisation.setLayoutY(170);
+        field_localisation.setPromptText("Facultatif");
+        field_description.setLayoutX(100); field_description.setLayoutY(270);
+        field_description.setMaxWidth(200);
+        field_description.setWrapText(true);
+        field_description.setPromptText("Facultatif");
+
         text_nom.setFont(new Font(20));
+        text_description.setFont(new Font(20));
+        text_localisation.setFont(new Font(20));
         text_date.setFont(new Font(20));
     }
 
     public void Creation_evenement_affiche(Group grp){
         Ajout_Date();
-        grp.getChildren().addAll(text_nom, nom_event, text_date, btn_suivant, btn_retour, btn_add_date, new_date);
+        grp.getChildren().addAll(text_nom, text_description, text_localisation, field_nom, field_description, field_localisation, text_date, btn_suivant, btn_retour, btn_add_date, new_date);
     }
 
     public void Creation_evenement_cache(Group grp){
-        grp.getChildren().removeAll(text_nom, nom_event, text_date, btn_suivant, btn_retour, btn_add_date);
+        grp.getChildren().removeAll(text_nom, text_description, text_localisation, field_nom, field_description, field_localisation, text_date, btn_suivant, btn_retour, btn_add_date);
         for (DatePicker date:calendar) {
             grp.getChildren().remove(date);
         }
@@ -72,7 +91,7 @@ public class Creation_evenement {
     public void Ajout_Date(){
         new_date = new DatePicker();
         new_date.setLayoutX(600); new_date.setLayoutY(btn_add_date.getLayoutY());
-        new_date.setPromptText("yyyy-MM-dd");
+        new_date.setPromptText("dd/MM/yyyy");
         calendar.add(new_date);
         btn_add_date.setLayoutY(btn_add_date.getLayoutY()+30);
         btn_add_date.setDisable(true);
